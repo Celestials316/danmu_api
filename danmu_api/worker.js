920a4b9f08992ab5529174ce3a9c11802439ee11
Wo254992 <110774618+Wo254992@users.noreply.github.com>
2025-11-22 15:24:19 +0800

    更新 worker.js

--- danmu_api/worker.js
+++ danmu_api/worker.js
@@ -1,4 +1,4 @@
-Import { Globals } from './configs/globals.js';
+import { Globals } from './configs/globals.js';
 import { exec } from 'child_process';
 import { promisify } from 'util';
 import fs from 'fs';
