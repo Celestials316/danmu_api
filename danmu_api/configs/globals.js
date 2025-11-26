1b0a162ebcf0b1eca0fded2269b7691af17b85f0
Celestials316 <lilixu254992@gmail.com>
2025-11-26 23:02:13 +0800

    更新 globals.js

--- danmu_api/configs/globals.js
+++ danmu_api/configs/globals.js
@@ -405,7 +405,7 @@ const Globals = {
    * 解析数据源顺序
    */
   parseSourceOrder(sourceOrder) {
-    const ALLOWED_SOURCES = ['360', 'vod', 'tmdb', 'douban', 'tencent', 'youku', 'iqiyi', 'imgo', 'bilibili', 'renren', 'hanjutv', 'bahamut'];
+    const ALLOWED_SOURCES = ['360', 'vod', 'tmdb', 'douban', 'tencent', 'youku', 'iqiyi', 'imgo', 'bilibili', 'renren', 'hanjutv', 'bahamut', 'dandan', 'sohu'];
     const orderArr = sourceOrder
       .split(',')
       .map(s => s.trim())
